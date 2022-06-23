d7cbb73b1e6b771ef9faa58fef3e7b7786e5a208
Nabil Izzullah <nabiel.p@students.amikom.ac.id>
2022-06-23 14:54:00 +0700

    adding what i learn

---
+++ 16_control_flow/defer.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+// Defer is used to let the compiler know that the deferred func is ran after every other script is done.
+// it is placed on the last stack. if you run more than 1 deferred func it is ran last in first out! be careful.
+// this happens because the deferred statement is stacked on top of the previous one, and called from the top.
+//pretty unique, huh?
+
+func defer1() {
+	fmt.Println("1")
+}
+
+func defer2() {
+	fmt.Println("2")
+}
+
+func main() {
+	defer func() {
+		fmt.Println("3")
+	}() //example of defer on IIFE
+	fmt.Println("Mulai")
+	defer defer2()
+	fmt.Println("Selesai")
+	defer defer1()
+}
